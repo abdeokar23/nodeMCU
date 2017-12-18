f7aac219e69557ef0e36564d24e9ff665abb0f0e
abdeokar23 <abdeokar23@gmail.com>
2017-12-18 14:54:19 +0530

    Initial commit

---
+++ mqtt_slider_pwm.ino
@@ -0,0 +1,102 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define LED_BUILTIN 16
+
+#define WIFI_SSID "ssid name here"
+#define WIFI_PASS "ssid passwd here"
+
+#define MQTT_SERV "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_NAME "your adafruit id here"
+#define MQTT_PASS "your adafruit token here"
+
+//Set up MQTT and WiFi clients
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERV, MQTT_PORT, MQTT_NAME, MQTT_PASS);
+
+//Set up the feed you're subscribing to
+Adafruit_MQTT_Subscribe slider = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/feeds/slider");
+
+void setup()
+{
+  Serial.begin(9600);
+
+  //Connect to WiFi
+  Serial.print("\n\nConnecting Wifi... ");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+
+  Serial.println("OK!");
+
+  //Subscribe to the onoff feed
+  mqtt.subscribe(&slider);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop()
+{
+  MQTT_connect();
+  
+  //Read from our subscription queue until we run out, or
+  //wait up to 5 seconds for subscription to update
+  Adafruit_MQTT_Subscribe * subscription;
+  while ((subscription = mqtt.readSubscription(5000)))
+  {
+    //If we're in here, a subscription updated...
+    
+    if (subscription == &slider)
+    {
+      //Print the new value to the serial monitor
+      Serial.print("value: ");
+      Serial.println((char*) slider.lastread);
+      int value = atoi((const char *)slider.lastread);  //converting the char received from adafruit to int type
+      
+      analogWrite(LED_BUILTIN, (1023-(value/10)*102));  //analog write for PWM active low logic
+      //highest value for pwm can be 1024 in our case, to divide it into 10 steps we use (1023-(value/10)*102)
+      //highest value 1023 is not reached in our code as we are not dividing by 102.4 precisely to avoid floating point operations
+    }
+  }
+
+  // ping the server to keep the mqtt connection alive
+  if (!mqtt.ping())
+  {
+    mqtt.disconnect();
+  }
+}
+
+
+void MQTT_connect() 
+{
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) 
+  {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) // connect will return 0 for connected
+  { 
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) 
+       {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
